4fe74b0464a2f8152bbfb04db11fe11138642029
adjtyh <522926840@qq.com>
2019-06-14 17:18:51 +0800

    未看增删改查

--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
 
+    private log(message: string) {
+      this.messageService.add(`HeroService: ${message}`);
+    }
+private heroesUrl = 'api/heroes';  // URL to web api
   /*getHeroes(): Hero[] {
     return HEROES;
   }
@@ -21,7 +27,11 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
-  
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
